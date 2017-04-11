7b4c692fb6d9cae229a4a97cebbd199a53b66372
Ben Mercer <ben@henroben.co.uk>
2017-04-11 16:08:18 +0100

    Types, validators and defaults added

--- server/server.js
+++ server/server.js
@@ -6,26 +6,49 @@ mongoose.Promise = global.Promise; // set mongoose to use promises
 mongoose.connect('mongodb://localhost:27017/TodoApp'); // connect to mongo database
 
 // create Todo model
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
+// let Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+//
+// let newTodo = new Todo({
+//     text: 'New test',
+// });
+//
+// newTodo.save().then((doc) => {
+//     console.log('Saved', doc);
+// }, (e) => {
+//     console.log('Unable to save');
+// });
+
+// create User model
+let User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }
 });
 
-let newTodo = new Todo({
-    text: 'Cook dinner',
-    completed: true,
-    completedAt: Date.now()
+let newUser = new User({
+    email: 'ben@henroben.co.uk'
 });
 
-newTodo.save().then((doc) => {
+newUser.save().then((doc) => {
     console.log('Saved', doc);
 }, (e) => {
-    console.log('Unable to save');
+    console.log('Unable to save', e);
 });
