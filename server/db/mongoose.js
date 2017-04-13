74ba7afe162dd2e6845c7a2a7b7621bb7a50a148
Ben Mercer <ben@henroben.co.uk>
2017-04-13 14:46:26 +0100

    remote db set up

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,10 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; // set mongoose to use promises
-mongoose.connect('mongodb://localhost:27017/TodoApp'); // connect to mongo database
+let db = {
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://todo-app-user:todo-app-user@ds159880.mlab.com:59880/todos-app'
+};
+mongoose.connect(db.mlab); // connect to mongo database
 
 module.exports = {mongoose};
