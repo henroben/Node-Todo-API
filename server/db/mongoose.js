bfafd7805b17642e072c351b27e98d19e4d84076
Ben Mercer <ben@henroben.co.uk>
2017-04-13 15:36:22 +0100

    config setup

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,12 @@
 "use strict";
 
 let mongoose = require('mongoose');
+let mongoConfig = require('./../config/config');
 
 mongoose.Promise = global.Promise; // set mongoose to use promises
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://todo-app-user:todo-app-user@ds159880.mlab.com:59880/todos-app'
+    mlab: `mongodb://${mongoConfig.username}:${mongoConfig.password}@ds159880.mlab.com:59880/todos-app`
 };
 mongoose.connect(db.mlab); // connect to mongo database
 
